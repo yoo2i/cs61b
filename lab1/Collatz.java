bdc0e9037e88fdb7067aef609ed5360d3975f823
yoo2i <3103165657@qq.com>
2024-01-09 17:36:21 +0800

    A buggy implementation of nextNumber in Collatz.java for Lab 4A

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,16 +3,16 @@
  */
 public class Collatz {
 
-    /** Correct implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if(n%2==1){
-            return n*3+1;
-        }
-        else{
-            return n/2;
-        }
+/** Buggy implementation of nextNumber! */
+public static int nextNumber(int n) {
+    if (n  == 128) {
+	return 1;
+    } else if (n == 5) {
+	return 3 * n + 1;
+    } else {
+	return n * 2;
     }
-
+}
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
